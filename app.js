1a9b360e51c1a8a17d616d5d6c62696b66864a68
Euak-Euak <79529743+Euak-Euak@users.noreply.github.com>
2025-04-19 14:33:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ app.get('/', (req, res) => {
     res.send('hello world!');
 });
 
+app.get('/ConnectServer', (req, res) => {
+    res.send('hello world!');
+});
+
+app.post('/UpdatePisition', (req, res => {
+    const { x, y } = req.body;
+
+    let result = {
+        message: String(x) + ' ' + String(y)
+    };
+
+    console.log(message);
+    res.send(result);
+});
+
 module.exports = app;
