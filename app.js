b85d38f9bfac25ea0e8959b6bf5d81cbb28a8731
Euak-Euak <79529743+Euak-Euak@users.noreply.github.com>
2025-04-26 11:40:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+let users1 = {};
+
+let users2 = {};
+
 app.get('/', (req, res) => {
     res.send('hello world!');
 });
@@ -16,10 +21,20 @@ app.post('/Login', (req, res) => {
 });
 
 app.post('/UpdatePosition', (req, res) => {
-    const { ID, Name, x, y } = req.body;
+    const { ID, Name, x, y, RoomNumber } = req.body;
 
     let message = 'ID' + String(ID) + ' User: ' + String(Name) + '    Move Position    X: ' + String(x) + ' Y: ' + String(y);
 
+    if(RoomNumber == 1)
+    {
+        users1[id].X = x;
+        users1[id].Y = y;
+    }
+    else(RoomNumber == 2)
+    {
+        users2[id].X = x;
+        users2[id].Y = y;
+    }
     console.log(message);
     res.send(message);
 });
@@ -31,6 +46,19 @@ app.post('/UpdateDirection', (req, res) => {
 
     console.log(message);
     res.send(message);
+});
+
+app.post('/UpdateData', (req, res) => {
+    const { RoomNumber } = req.body;
+
+    if(RoomNumber == 1)
+    {
+        res.send(users1);
+    }
+    else
+    {
+        res.send(users2);
+    }
 });
 
 module.exports = app;
